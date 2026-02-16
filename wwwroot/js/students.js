7588c1df9fd2608b7d8e1ef46036ad91a4db4e80
Danny <dhayek92@gmail.com>
2026-02-16 11:22:59 +0200

    Improved add student functionality on students page

--- wwwroot/js/students.js
+++ wwwroot/js/students.js
@@ -1,54 +1,5 @@
 ﻿document.addEventListener("DOMContentLoaded", async () => {
-
-    try {
-        const res = await axios.get("/students");
-
-        const students = res.data;
-
-        const studentGrades = students.map(student => {
-            let average = 0;
-            let num = 0;
-
-            const row = { id: student.id, name: student.name };
-            student.grades.forEach(grade => {
-                row[grade.course_Name] = grade.gradeValue;
-                average += grade.gradeValue;
-                num++;
-            });
-
-            row["Average"] = average / num;
-            return row;
-        });
-
-        console.log(studentGrades[0]);
-
-        $(function () {
-            $("#studentDataGrid").dxDataGrid({
-                dataSource: studentGrades,
-                columns: [
-                    { dataField: "id", caption: "ID", allowEditing: false },
-                    { dataField: "name", caption: "Student Name", allowEditing: false },
-                    {
-                        dataField: "Math", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }]
-                    },
-                    { dataField: "Physics", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
-                    { dataField: "Chemistry", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
-                    { dataField: "Biology", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
-                    { dataField: "English", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
-                    {
-                        dataField: "Average", allowEditing: false
-                    }
-                ],
-                editing: {
-                    mode: "popup",
-                    allowUpdating: true,
-                    allowDeleting: true,
-                },
-            });
-        });
-    } catch (e) {
-        DevExpress.ui.notify(e.message, "error");
-    }
+    getData();
 
     $(function () {
         $("#addStudentForm").dxForm({
@@ -164,6 +115,60 @@
             ]
         });
     });
+
+    async function getData() {
+        DevExpress.ui.notify("Fetching data...");
+
+        try {
+            const res = await axios.get("/students");
+
+            const students = res.data;
+
+            const studentGrades = students.map(student => {
+                let average = 0;
+                let num = 0;
+
+                const row = { id: student.id, name: student.name };
+                student.grades.forEach(grade => {
+                    row[grade.course_Name] = grade.gradeValue;
+                    average += grade.gradeValue;
+                    num++;
+                });
+
+                row["Average"] = average / num;
+                return row;
+            });
+
+            console.log(studentGrades[0]);
+
+            $(function () {
+                $("#studentDataGrid").dxDataGrid({
+                    dataSource: studentGrades,
+                    columns: [
+                        { dataField: "id", caption: "ID", allowEditing: false },
+                        { dataField: "name", caption: "Student Name", allowEditing: false },
+                        {
+                            dataField: "Math", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }]
+                        },
+                        { dataField: "Physics", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
+                        { dataField: "Chemistry", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
+                        { dataField: "Biology", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
+                        { dataField: "English", validationRules: [{ type: "range", min: 0, max: 100, message: "Grade must be 0–100" }] },
+                        {
+                            dataField: "Average", allowEditing: false
+                        }
+                    ],
+                    editing: {
+                        mode: "popup",
+                        allowUpdating: true,
+                        allowDeleting: true,
+                    },
+                });
+            });
+        } catch (e) {
+            DevExpress.ui.notify(e.message, "error");
+        }
+    }
 
     // Function to add a new student with their grades
     async function addStudent() {
