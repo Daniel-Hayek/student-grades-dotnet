033ab55adb29194f19215aaa04663c17d303361b
Danny <dhayek92@gmail.com>
2026-02-16 11:16:40 +0200

    Fixed an issue where notifications would show up twice because of a repeated viewport class definition

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -4,121 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Getting the various elements from the document to be used and manipulated
     const dataDiv = document.getElementById("dataDiv");
 
-    // Data for selectBox
-    const genderData = ["Male", "Female", "Other", "Prefer not to specify"];
-
-    // Data for tagBox
-    const dayData = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-    ];
-
-    // Number Box
-    $(function () {
-        $("#numberBoxContainer").dxNumberBox({
-            min: 0,
-            max: 100,
-            showSpinButtons: true,
-            label: "What is your favorite number?",
-            labelMode: "floating",
-            width: 300,
-        });
-    });
-
-    // Text Box
-    $(function () {
-        $("#textBoxContainer").dxTextBox({
-            label: "What is your name?",
-            labelMode: "floating",
-            width: 300,
-            showClearButton: true,
-        });
-    });
-
-    // Select Box
-    $(function () {
-        $("#selectBoxContainer").dxSelectBox({
-            dataSource: genderData,
-            label: "What is your gender?",
-            labelMode: "floating",
-            width: 300,
-        });
-    });
-
-    // Tag Box
-    $(function () {
-        $("#tagBoxContainer").dxTagBox({
-            dataSource: dayData,
-            label: "On which days do you work?",
-            labelMode: "floating",
-            width: 300,
-            multiline: true,
-            showSelectionControls: true,
-        });
-    });
-
-    // Button to handle data in boxes
-    $(function () {
-        $("#dataEntryButton").dxButton({
-            text: "Enter Data",
-            onClick: (e) => {
-                const num = $("#numberBoxContainer")
-                    .dxNumberBox("instance")
-                    .option("value");
-                const text = $("#textBoxContainer")
-                    .dxTextBox("instance")
-                    .option("value");
-                const tag = $("#tagBoxContainer").dxTagBox("instance").option("value");
-                const select = $("#selectBoxContainer")
-                    .dxSelectBox("instance")
-                    .option("value");
-
-                const popup = $("#dataPopup")
-                    .dxPopup({
-                        contentTemplate: () => {
-                            const content = $("<div />");
-
-                            content.append(
-                                $("<p />").text(`Your name: ${text || "No name given"}`),
-                                $("<p />").text(
-                                    `Your favorite number: ${num ?? "None selected"}`,
-                                ),
-                                $("<p />").text(`Your gender: ${select ?? "None selected"}`),
-                                $("<p />").text(
-                                    `Your workdays: ${tag.length != 0 ? tag : "None selected"}`,
-                                ),
-                            );
-
-                            return content;
-                        },
-                        hideOnOutsideClick: true,
-                        width: 400,
-                        height: 400,
-                        resizeEnabled: true,
-                        title: "Your Data",
-                    })
-                    .dxPopup("instance");
-
-                popup.show();
-
-                e.component.option("disabled", true);
-
-                setTimeout(() => {
-                    e.component.option("disabled", false);
-                }, 2000);
-            },
-            type: "normal",
-            stylingMode: "contained",
-            width: "240",
-            icon: "add",
-        });
-    });
-
     // DevExtreme Button
     $(function () {
         $("#buttonContainer").dxButton({
@@ -145,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
         DevExpress.ui.notify("Fetching data...");
 
         try {
+
             // Fetching and testing student data
             const res = await axios.get("/student-averages");
 
@@ -224,9 +110,11 @@ document.addEventListener("DOMContentLoaded", () => {
             });
 
             dataDiv.style.display = "block";
+
         } catch (e) {
             DevExpress.ui.notify(e.message);
         }
+
 
         // Creating chart
         try {
